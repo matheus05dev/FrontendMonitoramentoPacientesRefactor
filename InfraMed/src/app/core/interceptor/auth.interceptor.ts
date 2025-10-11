3cd27f9516915cd9b50ce65004014112bb83942c
Matheus Nunes da Silva <matheus05.nunes81@gmail.com>
2025-10-11 19:57:42 -0300

    correção de login

--- InfraMed/src/app/core/interceptor/auth.interceptor.ts
+++ InfraMed/src/app/core/interceptor/auth.interceptor.ts
@@ -6,7 +6,8 @@ import { TokenService } from '../services/token.service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
   const token = tokenService.getToken();
-  if (token) {
+  // Não adiciona Authorization no login
+  if (token && !req.url.endsWith('/api/auth/login')) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
